f8fc94fe9f94770ec31eaefb401342771e0c6755
roeuihyun <roeuihyun@gmail.com>
2021-07-31 14:18:37 +0900

    add increment and decrement method & binding onclick event

--- src/Counter.js
+++ src/Counter.js
@@ -9,14 +9,33 @@ class Counter extends Component{
             count : 5,
         }
     }
+
+    //
+    increment(){
+        this.setState({
+            //
+            count : this.state.count + 1,
+        })
+
+
+    }
+
+    //
+    decrement(){
+        this.setState({
+            //
+            count : this.state.count - 1,
+        })
+    }
+
     //
     render(){
         //
         return (
             <div>
-                <Button variant={"contained"} color={"primary"} size={"large"}> + </Button>
+                <Button variant={"contained"} color={"primary"} size={"large"} onClick={this.increment.bind(this)}> + </Button>
                 <Box component={"span"} margin={"5"}> {this.state.count} </Box>
-                <Button variant={"contained"} color={"primary"} size={"large"}> - </Button>
+                <Button variant={"contained"} color={"primary"} size={"large"} onClick={this.decrement.bind(this)}> - </Button>
             </div>
         )
     }
