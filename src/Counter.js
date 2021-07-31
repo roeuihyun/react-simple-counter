b43714a61ef71c486baa96b0b0ad67ad2f1d79fe
roeuihyun <roeuihyun@gmail.com>
2021-07-31 14:13:10 +0900

    add Constructor in Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -3,15 +3,22 @@ import { Box, Button } from "@material-ui/core";
 
 class Counter extends Component{
     //
+    constructor(){
+        super();
+        this.state = {
+            count : 5,
+        }
+    }
+    //
     render(){
         //
         return (
             <div>
                 <Button variant={"contained"} color={"primary"} size={"large"}> + </Button>
-                <Box component={"span"} margin={"5 "}> 0 </Box>
+                <Box component={"span"} margin={"5"}> {this.state.count} </Box>
                 <Button variant={"contained"} color={"primary"} size={"large"}> - </Button>
             </div>
-        );
+        )
     }
 }
 export default Counter;
